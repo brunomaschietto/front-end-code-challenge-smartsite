71005da16c6734cfe04038be40c4a447a06578ce
brunoMNasajon <brunomaschietto@nasajon.com.br>
2024-01-08 11:32:53 -0300

    fix: consertando um erro nos tipos

--- smartfit-project/src/contexts/GlobalContext.ts
+++ smartfit-project/src/contexts/GlobalContext.ts
@@ -3,6 +3,18 @@ import { academiaProp } from "../interfaces/types";
 
 interface GlobalContextProps {
     academias: academiaProp[];
+    sum: number;
+    setSum: (value: number) => void;
+    currentPage: number;
+    setCurrentPage: (page: number) => void;
+    totalPages: number;
 }
 
-export const GlobalContext = createContext<GlobalContextProps>({academias: []});
+export const GlobalContext = createContext<GlobalContextProps>({
+    academias: [],
+    sum: 0,
+    setSum: () => {},
+    currentPage: 1,
+    setCurrentPage: () => {},
+    totalPages: 1,
+});
